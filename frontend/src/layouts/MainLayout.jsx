9605183d2b07843f57cc0d8dcea49f9baab26adc
Romanchal <romanchalnayak@gmail.com>
2025-10-28 14:08:29 +0530

    layouts chnages

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -3,20 +3,28 @@ import Footer from "../components/common/Footer";
 import Sidebar from "../components/common/Sidebar";
 import { Outlet } from "react-router-dom";
 import "../styles/MainLayout.css";
+
 function MainLayout() {
   return (
-    <>
-        <div className="layout">
-            <Navbar />
-            <div className="main-content">
-                <Sidebar />
-                <div className="page-content">
-                <Outlet />
-                </div>
-            </div>
-            <Footer />
+    <div className="layout">
+      {/* Top Navbar */}
+      <Navbar />
+
+      <div className="main-wrapper d-flex">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Page Area */}
+        <div className="content-wrapper d-flex flex-column flex-grow-1">
+          <main className="page-content flex-grow-1 p-3">
+            <Outlet />
+          </main>
+
+          {/* Footer fixed at bottom */}
+          <Footer />
         </div>
-    </>
+      </div>
+    </div>
   );
 }
 
